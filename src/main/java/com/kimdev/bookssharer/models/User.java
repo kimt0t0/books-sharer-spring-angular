1cbfbfa7d89b31da310a759f946fa1e89e1f579b
Kim Robert <kimrobert.nb@gmail.com>
2024-01-05 17:56:16 +0100

    t001 feat: wip models

--- src/main/java/com/kimdev/bookssharer/models/User.java
+++ src/main/java/com/kimdev/bookssharer/models/User.java
@@ -1,13 +1,19 @@
 package com.kimdev.bookssharer.models;
 
+import org.hibernate.annotations.GenericGenerator;
+
 /**
  * @author: Kim Robert
  * @since: 05.01.2024
  */
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,14 +21,22 @@ import jakarta.persistence.Table;
 public class User {
 
     @Id
-    @GeneratedValue
-    private Integer id;
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private String id;
 
+    @Column(unique = true, nullable = false, updatable = false)
     private String username;
 
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(unique = true, nullable = false)
     private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "id_role")
+    private Role role;
 
     public User() {
 
